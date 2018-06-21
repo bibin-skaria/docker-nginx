11dc1b16b7ed848e7fb62974560745bf1cbfa372
bibin-skaria <bibin9992009@gmail.com>
2018-06-21 15:50:23 +0530

    Update ngnix-run.sh

--- ngnix-run.sh
+++ ngnix-run.sh
@@ -6,4 +6,4 @@ docker build --tag angular-nginx .
 
 # run angular-nginx for port 80 
 #***** please check default.conf file in current folder before executing
-docker run -d --name angular-nginx -p 80:80  --restart unless-stopped -v /opt/angular/www/html:/var/www/html angular-nginx:latest
+docker run -d --name angular-nginx -p 80:80  -p 443:443 --restart unless-stopped -v /opt/angular/www/html:/var/www/html angular-nginx:latest
